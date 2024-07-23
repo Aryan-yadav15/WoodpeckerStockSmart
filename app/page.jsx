0a68d865f0a139d63eb00174fa389d789088bce6
Aryan-yadav15 <a2003yadav@gmail.com>
2024-07-23 22:03:42 +0530

    Main

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,8 @@ import Link from "next/image";
 export default function Home() {
   return (
     <div className="p-10">
-      <div className="bg-gray-800 rounded-lg px-4 py-2">
-        <a href="/dashboard">
+      <div className="bg-gray-800 rounded-lg px-4 py-2 flex justify-center">
+        <a href="/dashboard" className="text-center text-white">
           Continue to dashboard
         </a>
       </div>
